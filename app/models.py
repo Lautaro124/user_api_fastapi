21e882195a298b52f5851ff6a3fbce275b71faf7
Lautaro Gonzalez <lautarogonzalez@CTL-DES-04.fibertel.com.ar>
2022-08-06 13:31:54 -0300

    first commit

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__='user'
+    id = Column(Integer, primary_key=True, index=True)
+    name= Column(String)
+    age= Column(Integer)
+    time_regist= Column(DateTime(timezone=True),server_default= func.now())
+    status_id= Column(Integer, ForeignKey('status_user.id'))
+
+    status = relationship("Status_user")
+
+class StatusUser(Base):
+    __tablename__='status_user'
+    id = Column(Integer, primary_key=True, index=True)
+    name= Column(String)
+    is_dangerous= Column(Boolean)
