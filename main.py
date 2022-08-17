db707f550fe50a37d32ab012e9bf69c93abd0142
Lautaro Gonzalez <lautarogonzalez@CTL-DES-04.fibertel.com.ar>
2022-08-17 17:49:51 -0300

    Cors

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from dotenv import load_dotenv
 
 from routes import router_index
 
 
-load_dotenv('.env')
+load_dotenv(".env")
 
 tag_metadata= [
     {"name": "👤 User", "description": "endpoint user data"},
@@ -17,12 +18,20 @@ tag_metadata= [
 app = FastAPI(
     openapi_tags=tag_metadata,
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 app.include_router(router_index.router)
 
-@app.get('/')
+@app.get("/")
 async def initialize():
-    return {'message': 'Iniciado'}
+    return {"message": "Iniciado"}
 
 
